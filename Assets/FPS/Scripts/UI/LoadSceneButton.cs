03e0134fd8fdac9125a48f419ab04524e0dc3568
Ivan <ivan.kashytski.mkx@gmail.com>
2021-10-28 21:08:33 +0300

    5_levels_one_by_one

---
+++ Assets/FPS/Scripts/UI/LoadSceneButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Unity.FPS.Game;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace Unity.FPS.UI
+{
+    public class LoadSceneButton : MonoBehaviour
+    {
+        List<string> scenes = new List<string> {"Level_1", "Level_2", "Level_3", "Level_4", "Level_5"}; 
+
+        void Update()
+        {
+            if (EventSystem.current.currentSelectedGameObject == gameObject
+                && Input.GetButtonDown(GameConstants.k_ButtonNameSubmit))
+            {
+                LoadTargetScene();
+            }
+        }
+
+        public void LoadTargetScene()
+        {
+            string currScene = PlayerPrefs.GetString("scene");
+            int numScene = scenes.IndexOf(currScene) + 1;
+            if (numScene == 5) numScene = 1;
+            SceneManager.LoadScene(scenes[numScene]);
+        }
+    }
+}
